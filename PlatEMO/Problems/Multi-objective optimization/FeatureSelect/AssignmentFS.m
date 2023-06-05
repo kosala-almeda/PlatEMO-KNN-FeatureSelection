dbfd6039ab0192b776faadae01978e52027c4ad8
Kosala Almeda <kosala86@hotmail.com>
2023-06-05 01:00:37 -0400

    K-fold cross validation

--- PlatEMO/Problems/Multi-objective optimization/FeatureSelect/AssignmentFS.m
+++ PlatEMO/Problems/Multi-objective optimization/FeatureSelect/AssignmentFS.m
@@ -1,7 +1,10 @@
 classdef AssignmentFS < PROBLEM
 % <multi> <binary> <large/none> <expensive/none> <sparse/none>
 % The feature selection problem for KNN classification
-% dataSetNo --- 1 --- Data set number (1-Hillvally, 2-musk, 3-Madelon, 4-Movement)
+% dataSetNo --- 4 --- Data set number (1-WBCD, 2-Sonar, 3-Movement, 4-Hillvally, 5-Musk1, 6-Multiple(pix), 7-Arrhythmia, 8-Madelon)
+% Kf        --- 4 --- Number of folds for KFold cross validation
+% Kn        --- 5 --- Number of nearest neighbors for KNN classification
+% trainSize --- 0.7 --- Portion of data used for training
 
 %------------------------------- Reference --------------------------------
 % Y. Tian, X. Zhang, C. Wang, and Y. Jin, An evolutionary algorithm for
@@ -21,97 +24,65 @@ classdef AssignmentFS < PROBLEM
 
 % The datasets are taken from the penfwang repository in
 %  https://github.com/penfwang/Inf_Sci_MODE
-% No.   Name                              Samples Features Classes  Type
-% 1     Hillvally                          1212     100       2     Real
-% 2     Musk1                              2031     166       2     Integer
-% 3     Madelon                            2600     500       2     Real
-% 4     Movement                            360      90      15     Real
-
+%
+% The information of datasets
+% Index     Dataset        # Features   # Classes   # Instances     Feature Type    Area
+%     1     WBCD                   30           2           569     Real            Medicine
+%     2     Sonar                  60           2           208     Real            Physics
+%     3     Movement               90          15           360     Real            Medicine
+%     4     Hillvally             100           2          1212     Real            Graph
+%     5     Musk1                 166           2          2031     Integer         Physics
+%     6     Multiple (pix)        240          10          2000     Mixed           Digit Recognition
+%     7     Arrhythmia            279          16           452     Mixed           Medicine
+%     8     Madelon               500           2          2600     Real            Artificial Dataset
 
     properties(Access = private)
         TrainIn;    % Input featues of training set
         TrainOut;   % Output class of training set
-        ValidIn;    % Input featues of validation set
-        ValidOut;   % Output class of validation set
         TestIn;     % Input featues of test set
         TestOut;    % Output class of test set
         Category;   % Class labels
+        Kf;         % Number of folds for KFold cross validation
+        Kn;         % Number of nearest neighbors for KNN classification
     end
     methods
         %% Default settings of the problem
         function Setting(obj)
+            % Parameter setting
+            [dataSetNo, obj.Kf, obj.Kn,  trainSize] = obj.ParameterSet(4, 4, 5, 0.7);
+
             % Load data
-            dataSetNo = obj.ParameterSet(1);
-            switch dataSetNo
-                case 1
-                    disp('Hill_Valley Data set: Features: 100, Classes: 2');
-                    Data1 = importdata('Hill_Valley_without_noise_Training.data').data;
-                    Data2 = importdata('Hill_Valley_without_noise_Testing.data').data;
-                    fullDataSet = vertcat(Data1, Data2);
+            [inputs,output] = loadDataSet(dataSetNo);
 
-                case 2
-                    disp('Musk1 Data set: Features: 166, Classes: 2');
-                    file = importdata('musk.csv');
-                    fullDataSet = file.data;
-
-                case 3
-                    disp('Madelon Data set: Features: 500, Classes: 2');
-                    file = importdata('madelon.csv');
-                    fullDataSet = file.data;
-
-                case 4
-                    disp('Movement_libras Data set: Features: 90, Classes: 15');
-                    fullDataSet = importdata('movement_libras.data');
-            end
+            % shuffle data
+            randIdx = randperm(size(inputs, 1));
+            inputs = inputs(randIdx, :);
+            output = output(randIdx, :);
 
             % reduce data set to 1000 rows 
-            if size(fullDataSet, 1) > 1000
-                selectedDataSet = fullDataSet(randperm(size(fullDataSet, 1), 1000), :);
-            else
-                selectedDataSet = fullDataSet;
+            if size(inputs, 1) > 1000
+                inputs = inputs(1:1000, :);
+                output = output(1:1000, :);
             end
 
-            [train, valid, test] = obj.segmentData(selectedDataSet);
-            [trInput, trOutput] = obj.normalizeData(train);
-            [vaInput, vaOutput] = obj.normalizeData(valid);
-            [teInput, teOutput] = obj.normalizeData(test);
+            inputs = normalizeData(inputs);
+            obj.Category = unique(output);
 
-            obj.Category    = unique(trOutput(:,end));
-            obj.TrainIn     = trInput;
-            obj.TrainOut    = trOutput;
-            obj.ValidIn     = vaInput;
-            obj.ValidOut    = vaOutput;
-            obj.TestIn      = teInput;
-            obj.TestOut     = teOutput;
+            % Split data into training and test sets
+            obj.TrainIn     = inputs(1:ceil(end*trainSize),:);
+            obj.TrainOut    = output(1:ceil(end*trainSize),:);
+            obj.TestIn     = inputs(ceil(end*trainSize)+1:end,:);
+            obj.TestOut    = output(ceil(end*trainSize)+1:end,:);
 
             % Parameter setting
             obj.M        = 2; % Number of objectives (Number of selected features, Validation error)
             obj.D        = size(obj.TrainIn,2); % Number of decision variables (Number of features in the dataset)
             obj.encoding = 4 + zeros(1,obj.D); % Encoding design (4: binary encoding)
-        end
 
-        % Segmantation (train, validation, test)
-        function [train, valid, test] = segmentData(obj,dataSet)
-            numRows = size(dataSet, 1);  
-            % Training data test 50% 
-            indicesTrain = randperm(numRows, round(numRows * 0.50));
-            train = dataSet(indicesTrain, :);  
-            % Validation data test 20% 
-            remainingIndices = setdiff(1:numRows, indicesTrain);  
-            remainingData = dataSet(remainingIndices, :); 
-            indicesValid = randperm(size(remainingData, 1), round(numRows * 0.20));  
-            valid = remainingData(indicesValid, :);
-            % Test data test 30% 
-            test = setdiff(remainingData, valid, 'rows'); 
-        end
-
-        % Normalization (min-max normalization)
-        function [input, output] = normalizeData(obj, Data)
-            Fmin = min(Data(:,1:end-1),[],1);
-            Fmax = max(Data(:,1:end-1),[],1);
-            Data(:,1:end-1) = (Data(:,1:end-1)-repmat(Fmin,size(Data,1),1))./repmat(Fmax-Fmin,size(Data,1),1);
-            input = Data(:,1:end-1);
-            output = Data(:,end);
+            % Classification error using all features
+            result = CalObj(obj,ones(1,obj.D));
+            display(['Classification error using all features: ', num2str(result(1))])
+            disp('===============================================')
         end
 
         %% Calculate objective values
@@ -119,19 +90,44 @@ classdef AssignmentFS < PROBLEM
             PopDec = logical(PopDec);
             PopObj = zeros(size(PopDec,1),2);
             for i = 1 : size(PopObj,1)
-                % Rank the training samples according to their distances to the current solution
-                [~,Rank] = sort(pdist2(obj.ValidIn(:,PopDec(i,:)),obj.TrainIn(:,PopDec(i,:))),2);
-                % Predict the labels by the majority voting of K(=5) Nearest Neighbors
-                %   mode is not used as following strategy is better at tie breaking
-                %   it gives priority close neighbors in case of a tie
-                [~,Out]  = max(hist(obj.TrainOut(Rank(:,1:5))',obj.Category),[],1); 
-                Out      = obj.Category(Out);
-                % Using mean over sum to normalize the objective value between 0 and 1
-            	PopObj(i,1) = mean(PopDec(i,:));
-                % Validation error is the ratio of misclassified samples
-                PopObj(i,2) = mean(Out~=obj.ValidOut);
+
+                % KFold cross validation using Training set
+                foldSize = ceil(size(obj.TrainIn,1)/obj.Kf);
+                % split to Kf folds, for each fold, train and test
+                for j = 1:obj.Kf
+                    % get training and test data
+                    if j == 1
+                        trainIn = obj.TrainIn(foldSize+1:end, :);
+                        trainOut = obj.TrainOut(foldSize+1:end, :);
+                        validIn = obj.TrainIn(1:foldSize, :);
+                        validOut = obj.TrainOut(1:foldSize, :);
+                    elseif j == obj.Kf
+                        trainIn = obj.TrainIn(1:(j-1)*foldSize, :);
+                        trainOut = obj.TrainOut(1:(j-1)*foldSize, :);
+                        validIn = obj.TrainIn((j-1)*foldSize+1:end, :);
+                        validOut = obj.TrainOut((j-1)*foldSize+1:end, :);
+                    else
+                        trainIn = obj.TrainIn([1:(j-1)*foldSize, j*foldSize+1:end], :);
+                        trainOut = obj.TrainOut([1:(j-1)*foldSize, j*foldSize+1:end], :);
+                        validIn = obj.TrainIn((j-1)*foldSize+1:j*foldSize, :);
+                        validOut = obj.TrainOut((j-1)*foldSize+1:j*foldSize, :);
+                    end
+                    
+
+                    % Rank the training samples according to their distances to the current solution
+                    [~,Rank] = sort(pdist2(validIn(:,PopDec(i,:)),trainIn(:,PopDec(i,:))),2);
+                    % Predict the labels by the majority voting of K Nearest Neighbors
+                    %   mode is not used as following gives priority close neighbors in case of a tie
+                    [~,Out]  = max(hist(trainOut(Rank(:,1:obj.Kn))',obj.Category),[],1);
+                    Out      = obj.Category(Out);
+                    % Using mean over sum to normalize the objective value between 0 and 1
+                    PopObj(i,1) = mean(PopDec(i,:));
+                    % Validation error is the ratio of misclassified samples
+                    PopObj(i,2) = mean(Out~=validOut);
+                end
             end
         end
+
         %% Display a population in the objective space
         function DrawObj(obj,Population)
             % Rescale the objective values
@@ -144,7 +140,7 @@ classdef AssignmentFS < PROBLEM
             plot(ax,ParetoObj(bi,1),ParetoObj(bi,2),'k.');
             ax.XLim = [0,max(PopObj(:,1))+1];
             ax.YLim = [0,max(PopObj(:,2))+1];
-            legend(ax,'Solutions','Pareto front', 'MCE', 'Location', 'SouthWest');
+            legend(ax,'Other Solutions','Pareto front', 'MCE', 'Location', 'SouthWest');
         end
         %% Display a population with post optimization test results
         function DrawTest(obj,Population)
@@ -166,7 +162,7 @@ classdef AssignmentFS < PROBLEM
             plot(ax,ParetoObj(bi,1),ParetoObj(bi,2),'k.');
             ax.XLim = [0,max(PopObj(:,1))+1];
             ax.YLim = [0,max(PopObj(:,2))+1];
-            legend(ax,'Solutions', 'Original Pareto','Non dominated', 'MCE', 'Location', 'SouthWest');
+            legend(ax,'Other Solutions', 'Original Pareto','Non dominated', 'MCE', 'Location', 'SouthWest');
         end
         %% Display a population in the decision space
         function DrawDec(obj,Population)
@@ -176,8 +172,8 @@ classdef AssignmentFS < PROBLEM
             pi = NDSort(PopObjs,1) == 1;
 
             PopDecs = Population.decs;
-            C = zeros(size(PopDecs)) + 0.7;
-            C(pi,:) = zeros+0.4;
+            C = zeros(size(PopDecs)) + 0.8;
+            C(pi,:) = zeros+0.5;
             C(bi,:) = zeros;
             C(~PopDecs) = 1;
             surf(ax,ones(size(PopDecs')),repmat(C',1,1,3),'EdgeColor','none');
@@ -214,13 +210,56 @@ classdef AssignmentFS < PROBLEM
                 end
             end
             disp('For training (validation) data:');
-            display(['MCE: ',num2str(mce(1)), ' ,   No of Features: ',num2str(sum(mcesolution))]);
+            display(['MCE: ',num2str(mce(1)), ' ,   No of Features: ',num2str(sum(mcesolution(1,:)))]);
             display(['Feature set: ',num2str(find(mcesolution(1,:)))]);
             display(['HV: ', num2str(obj.CalMetric('HV',Population))])
             disp('For test data:');
-            display(['MCE: ',num2str(mce(2)), ' ,   No of Features: ',num2str(sum(mcesolution))]);
+            display(['MCE: ',num2str(mce(2)), ' ,   No of Features: ',num2str(sum(mcesolution(2,:)))]);
             display(['Feature set: ',num2str(find(mcesolution(2,:)))]);
             disp('===============================================')
         end
     end
 end
+
+% Get the data set
+function [features, classes] = loadDataSet(dataSetNo)
+    switch dataSetNo
+        case 3
+            disp('Movement_libras Data set: Features: 90, Classes: 15');
+            dataSet = importdata('movement_libras.data');
+            features = dataSet(:, 1:end-1);
+            classes = dataSet(:, end);
+        case 4
+            disp('Hill_Valley Data set: Features: 100, Classes: 2');
+            trainData = importdata('Hill_Valley_without_noise_Training.data').data;
+            testData = importdata('Hill_Valley_without_noise_Testing.data').data;
+            dataSet = vertcat(trainData, testData);
+            features = dataSet(:, 1:end-1);
+            classes = dataSet(:, end);
+        case 5
+            disp('Musk1 Data set: Features: 166, Classes: 2');
+            file = importdata('musk.csv');
+            dataSet = file.data;
+            features = dataSet(:, 4:end-1);
+            classes = dataSet(:, end);
+        case 8
+            disp('Madelon Data set: Features: 500, Classes: 2');
+            file = importdata('madelon.csv');
+            dataSet = file.data;
+            features = dataSet(:, 1:end-1);
+            classes = dataSet(:, end);
+        otherwise
+            assert(false, 'Invalid data set number');
+    end
+    disp('-----------------------------------------------');
+end
+
+% Normalization (min-max normalization)
+function [input, output] = normalizeData(Data)
+    Fmin = min(Data(:,1:end-1),[],1);
+    Fmax = max(Data(:,1:end-1),[],1);
+    Data(:,1:end-1) = (Data(:,1:end-1)-repmat(Fmin,size(Data,1),1))./repmat(Fmax-Fmin,size(Data,1),1);
+    input = Data(:,1:end-1);
+    output = Data(:,end);
+end
+
